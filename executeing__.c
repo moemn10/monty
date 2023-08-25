a371bb627386f834cb47ec90c23f97b7c862628a
moemn10 <moemnkoko@gmail>
2023-08-25 11:23:12 -0700

    moemn

--- executeing__.c
+++ executeing__.c
@@ -1,23 +1,23 @@
 #include "monty.h"
 
 /**
- * is_emptyl -testing if the line empty or not
+ * is_emp - check if  the line empty or not
  * @l: the line that will be checked
- * Return: 1 line is empty, 0 not empty
+ * Return: 1 if line is empty.
  */
 
 int is_emptyl(char *l)
 {
-	int leng = strlen(l);
-	int i;
+	int len = strlen(l);
+	int n;
 
-	if (leng > 0)
+	if (len > 0)
 	{
-		if (l[leng - 1] == '\n')
+		if (l[len - 1] == '\n')
 		{
-			for (i = 0; i < leng - 1; i++)
+			for (n = 0; n < len - 1; n++)
 			{
-				if (l[i] != ' ')
+				if (l[n] != ' ')
 					return (1);
 			}
 			return (0);
@@ -28,82 +28,82 @@ int is_emptyl(char *l)
 
 
 /**
- * execf- executes operations in file
- * @fn: name of file that has the operations
- * Return: 0 success, -1 failure
+ * db_ecf- executing operations in file
+ * @fn: name of the file that has the operations
+ * Return: 0 if success, if -1 failure
  */
 int execf(char *fn)
 {
-	char *l = NULL;
+	char *o = NULL;
 	FILE *file;
-	ssize_t read;
-	size_t lleng = 0;
+	ssize_t reading;
+	size_t len = 0;
 	data_t data;
 
 	data.stack = NULL;
 	file = fopen(fn, "r");
 	if (file == NULL)
-		open_file_error(fn);
+		open_ferror(fn);
 	data.file = file;
-	while ((read = getline(&l, &lleng, file)) != -1)
+	while ((reading = getline(&o, &len, file)) != -1)
 	{
-		data.line_number++;
-		data.l = l;
-		if (is_emptyl(data.l) == 1)
-			execl(data.l, data.line_number);
+		data.line_num++;
+		data.o = o;
+		if (is_emp(data.o) == 1)
+			bd_ecl(data.o, data.line_num);
 	}
-	free_stack(data.stack);
+	data_stack(data.stack);
 	fclose(file);
-	free(l);
+	free(o);
 	return (0);
 }
 
 
 /**
- * execl- executes a single operation's line
+ * db_ecl- executing a single operations line
  * @l: operation's line
  * @line_number: current operation's line number
  */
-void execl(char *l, int line_number)
+void db_ecl(char *l, int line_num)
 {
-	int is_inst = -1;
-	int i;
+	int is_in = -1;
+	int n;
 	data_t data;
 	instruction_t insts[] = {
-		{"push", push},
-		{"sub", sub},
-		{"div", the_div},
-		{"add", add},
-		{"mul", mul},
-		{"mod", mod},
-		{"pstr", pstr},
-		{"rotl", rotl},
-		{"rotr", rotr},
-		{"pop", pop},
-		{"pall", pall},
-		{"pint", pint},
-		{"swap", swap},
-		{"nop", nop},
-		{"pchar", pchar},
-		{"queue", queue},
-		{"stack", stack},
+		{"push", _push},
+		{"sub", _sub},
+		{"div", _the_div},
+		{"add", _add},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{"pop", _pop},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"swap", _swap},
+		{"nop", _nop},
+		{"pchar", _pchar},
+		{"queue", _queue},
+		{"stack", _stack},
 	};
 	data.arg1 = strtok(l, " \n");
 	data.arg2 = strtok(NULL, " \n");
 	if (data.arg1[0] == '#')
 	{
-		nop(&data.stack, line_number);
+		_nop(&data.stack, line_num);
 		return;
 	}
-	for (i = 0; i < INSTRUCTIONS_COUNT; i++)
-		if (strcmp(data.arg1, insts[i].opcode) == 0)
+	for (n = 0; n < INSTRUCTIONS_COUNT; n++)
+		if (strcmp(data.arg1, insts[n].opc) == 0)
 		{
 
-			is_inst = i;
+			is_in = n;
 			break;
 		}
-	if (is_inst != -1)
-		insts[i].f(&data.stack, line_number);
+	if (is_in != -1)
+		insts[n].f(&data.stack, line_num);
 	else
-		pinvalid_instruction(line_number, data.arg1);
+		p_val(line_num, data.arg1);
 }
